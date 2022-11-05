6f9e55e628e0a2e5d0a274be3dc46b8e487f29e9
Mohandes Jiri <mohandes.jiri@gmail.com>
2022-11-05 23:33:20 +0330

    Change api urls

--- src/addresses.ts
+++ src/addresses.ts
@@ -3,6 +3,6 @@ export default {
 	"MAYAN_EVM_CONTRACT": "0xEAE8425c60B12D09D12d02ff981F85b34f2dCFcc",
 	"TOKEN_PROGRAM_ID": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
 	"ASSOCIATED_TOKEN_PROGRAM_ID": "ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL",
-	"EXPLORER_URL": "https://price-api.mayan.finance",
-	"PRICE_URL": "https://price-api.mayan.finance"
+	"EXPLORER_URL": "https://price-api.mayan.finance/v1",
+	"PRICE_URL": "https://price-api.mayan.finance/v1"
 }
