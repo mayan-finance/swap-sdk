5b67c45c888ce6a0b122e03533f3971afbf3f128
Mohandes Jiri <mohandes.jiri@gmail.com>
2023-05-11 01:58:42 +0330

    Export addresses

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+import mayanAddresses from './addresses';
 export * from './api';
 export * from './evm';
 export * from './solana';
 export * from './types'
+export const addresses = mayanAddresses;
