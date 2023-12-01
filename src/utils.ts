66e34fbb954967607e201b41775f79b24711aabe
mohandesjiri <mohandes.jiri@gmail.com>
2023-12-01 18:37:25 +0300

    Update version

--- src/utils.ts
+++ src/utils.ts
@@ -102,7 +102,7 @@ export function getWormholeChainIdById(chainId: number) : number | null {
 	return evmChainIdMap[chainId];
 }
 
-const sdkVersion = [4, 4, 2];
+const sdkVersion = [4, 4, 3];
 
 export function checkSdkVersionSupport(minimumVersion: [number, number, number]): boolean {
 	//major
