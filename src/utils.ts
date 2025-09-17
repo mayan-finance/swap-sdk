93278dc246ef35926614c50caec5da17d75b8ffd
Jiri <mohandes.jiri@gmail.com>
2025-09-17 20:58:41 +0300

    Add HyperEVM for Fast Mctp

--- src/utils.ts
+++ src/utils.ts
@@ -161,7 +161,7 @@ export function getWormholeChainIdById(chainId: number) : number | null {
 	return evmChainIdMap[chainId];
 }
 
-const sdkVersion = [11, 0, 1];
+const sdkVersion = [11, 1, 0];
 
 export function getSdkVersion(): string {
 	return sdkVersion.join('_');
