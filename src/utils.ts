2c6031062d401b60c5a2824e1e777d7ee342e768
Jiri <mohandes.jiri@gmail.com>
2026-04-08 14:50:47 +0300

    Version 13.3.0

--- src/utils.ts
+++ src/utils.ts
@@ -169,7 +169,7 @@ export function getWormholeChainIdById(chainId: number) : number | null {
 	return evmChainIdMap[chainId];
 }
 
-const sdkVersion = [13, 2, 0];
+const sdkVersion = [13, 3, 0];
 
 export function getSdkVersion(): string {
 	return sdkVersion.join('_');
