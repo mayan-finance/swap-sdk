accdbd7019060f0727a8d537a4871cb36d188c2a
Jiri <mohandes.jiri@gmail.com>
2026-08-16 12:47:46 +0300

    Fix Solana monochain swap validation

--- src/utils.ts
+++ src/utils.ts
@@ -180,7 +180,7 @@ export function getWormholeChainIdById(chainId: number) : number | null {
 	return evmChainIdMap[chainId];
 }
 
-const sdkVersion = [15, 2, 0];
+const sdkVersion = [15, 2, 1];
 
 export function getSdkVersion(): string {
 	return sdkVersion.join('_');
